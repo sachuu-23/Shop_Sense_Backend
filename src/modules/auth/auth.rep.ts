dc506d94efded1dcfaea95b75227ade88d6c5e32
Sachin <sachinmizo1223@gmail.com>
2026-03-13 20:57:51 +0530

    Done some changes

--- src/modules/auth/auth.rep.ts
+++ src/modules/auth/auth.rep.ts
@@ -1,43 +1,56 @@
 import pool from "../../config/db";
+//do create the interface if there are multiple things coming as an parmater in our input data
+//in ts this is also called as type definition where we have to before hand defined that what kind of data is coming and only those values can come which its set in interface
 
-//Interface to the data which we are rechiving or the types of data which we will receive we have to define that in out interface.
-interface CreateUserInput {
+interface CreateUserByInput {
     username : string,
-    email  : string,
+    email : string ,
     password : string
-};
-interface FindUser{
-    email : string
 }
 
-//fucnction to insert the user details and mark it export , because we will call this function from other file with data.
- export const createUser = async (data : CreateUserInput)=>{
+interface refreshTokenInput {
+    user_id :string,
+    token_hash : string,
+    expires_at : string
+}
+
+
+export const createUser = async (data : CreateUserByInput)=>{
     const result = await pool.query(
-        `INSERT INTO users (username,email,password_hash)
-         VALUES($1,$2,$3) RETURNING *`,
-         [
+        `INSERT INTO users (username, email, pasword_hash)
+        VALUES($1,$2,$3) RETURNING *`,
+        [
             data.username,
             data.email,
             data.password
-         ]
+        ]
     );
     return result.rows[0];
 };
-
-//Now this function is for findingt the user using email 
-//for select there is no need to write returning
 
 export const findUserByEmail = async (email : string)=>{
     const result = await pool.query(
         `SELECT * FROM users 
-         WHERE email = $1`,
+        WHERE email = $1 `,
         [
-            data.email
+            email
         ]
+    );//never forget to keep semicolon here 
 
+    return result.rows[0];
+};
+
+export const saveRefreshToken = async (data : refreshTokenInput)=>{
+    const result = await pool.query(
+        `INSERT INTO refresh_token (user_id, token_hash, expires_at)
+        VALUES($1,$2,$3) RETURNING *`,
+        [
+            data.user_id,
+            data.token_hash,
+            data.expires_at
+
+        ]
         
     );
     return result.rows[0];
 }
-
-
