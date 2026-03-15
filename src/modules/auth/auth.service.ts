0b1b302039b0b673c74b0e46e4f5182eb910b399
Sachin <sachinmizo1223@gmail.com>
2026-03-15 23:56:01 +0530

    Done some changes

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,17 +2,24 @@
 import {UserInput} from "./auth.types";
 import {findUserByEmail,createUser} from "./auth.repo";
 import bcrypt from "bcrypt";
+import jwt from  "jsonwebtoken";
 const SaltRound = 12;// this is a salt constant that doesnt change , which means a config kind of thing , this salt is important from hashing password.
 export const registerUser = async (data : UserInput)=>{
     try{
+
+        //First check does the user even exist
         const existingUserCheck = await findUserByEmail(data.email);
      
     if(existingUserCheck){
         throw new Error("User Already Exist");
     }
+      
 
+    //Then hash the password
     const HashPassword = await bcrypt.hash(data.password,SaltRound);
     
+
+    //Create a new input, which will be passed in repo as UserInput 
     const newUserData = {
         username : data.username,
         email : data.email,
@@ -20,7 +27,6 @@ export const registerUser = async (data : UserInput)=>{
     }
 
     const registerUsers = await createUser(newUserData);
-
-
-    }
+    const jwt_signature = jwt.sign({user_id : registerUsers.user_id},process.env.JWT_SECRET!,{expiresIn: "15m"});
+}
 }
