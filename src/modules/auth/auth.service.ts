e88aa08958424187c935996bceb99b9d6c7d28ea
Sachin <sachinmizo1223@gmail.com>
2026-03-16 12:21:21 +0530

    Done some changes

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,9 @@
 //6
 import {UserInput} from "./auth.types";
-import {findUserByEmail,createUser} from "./auth.repo";
+import {findUserByEmail,createUser,saveRefreshToken} from "./auth.repo";
 import bcrypt from "bcrypt";
 import jwt from  "jsonwebtoken";
+import crypto from "crypto";
 const SaltRound = 12;// this is a salt constant that doesnt change , which means a config kind of thing , this salt is important from hashing password.
 export const registerUser = async (data : UserInput)=>{
     try{
@@ -13,6 +14,9 @@ export const registerUser = async (data : UserInput)=>{
     if(existingUserCheck){
         throw new Error("User Already Exist");
     }
+
+
+    //there are three main concepts in auth servcie layer , which is bcrypt, jwt and crypt for refresh token genration
       
 
     //Then hash the password
@@ -25,8 +29,33 @@ export const registerUser = async (data : UserInput)=>{
         email : data.email,
         password : HashPassword
     }
+      
 
+    //here in registersUser we will get back userId which we will use further 
     const registerUsers = await createUser(newUserData);
     const jwt_signature = jwt.sign({user_id : registerUsers.user_id},process.env.JWT_SECRET!,{expiresIn: "15m"});
+    
+
+    //here we generate refresh token using crypto , but we never store raw token in out database , we need to hash it but using diff hashing concepts.
+
+    const refresh_token = crypto.randomBytes(32).toString("hex");
+
+    //random bytes we choose to be 32 = 256 bits of});
+//whcih outputs the data as 256 bits , so there is no use of geenrating of large values.
+
+    const hashed_refresh_token = crypto.createHash("sha256").update(refresh_token).digest("hex");
+
+    const expiresAt  = new Date(Date.now() + 7*24*60*60*1000);
+
+    const result = await saveRefreshToken({
+        user_id : registerUsers.user_id,
+        token_hash : hashed_refresh_token, 
+        expires_at : expiresAt });
+
+        return {
+            accessToken : jwt_signature,
+            refreshToken :
+        }
+
 }
-}
+} catch()
