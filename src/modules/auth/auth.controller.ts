0c237ed2ee00a24a8fc07a7471288e16643ed71c
Sachin <sachinmizo1223@gmail.com>
2026-03-18 22:54:31 +0530

    Done some changes

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,36 @@
+//In this layer, our task is to take input which is coming from the user and extracting important stuffs according to the requirment and then forwarding it to service layer.
+//This layer also takes care of rsponse and diff status codes.
+
+import {Request, Response, NextFunction} from "express";
+import {registerUser} from "./auth.service";
+
+const RegisterUser = async(req:Request, res:Response)=>{
+    try{
+        const {username, email, password} = req.body;
+        //here we have used the logic of destructuring , whcich means we extract username , email and password from our body.
+        //the other method is 
+        //const body = req.body;
+         //body.username and body.email and body.password
+
+
+      const result = await registerUser({username, email,password});
+      res.status(201).json({result});
+
+    }catch(error){
+        if(error instanceof Error && error.message === "User Already Exist"){
+            res.status(409).json({
+                message : "User Already Exist"});
+                //always send json data in the form of key and value.
+
+        }else{
+            res.status(500).json({
+                message : "Unexpected Server Error"
+            });
+            
+
+        }
+    }
+
+};
+
+//if you are sending an error 
