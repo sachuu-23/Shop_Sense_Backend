3dde27fbab9e4b73a9f0f3d9285b6ddf0ed91e19
Sachin <sachinmizo1223@gmail.com>
2026-03-26 02:10:30 +0530

    Done some changes

---
+++ src/middleware/auth.zodValidation.js
@@ -0,0 +1,21 @@
+import {z} from "zod";
+
+
+//RegisterUser Zod Schema 
+export const RegisterUserSchema = z.object({
+    username : z.string().min(3,"Username must be atleast 3 character")
+    .max(30,"Username shouldn't exceeds 30 character"),
+
+    email : z.string().email().trim().toLowerCase(),
+
+    password : z.string().min(8,"Password must be atleast 8 character").max(100,"Password shouldn't exceeds 100 character")
+
+});
+
+
+//LoginUser Zod Schema
+export const LoginUserSchema = z.object({
+    email : z.string().email().trim().toLowerCase(),
+    password : z.string().min(8,"Password must be ateast 8 character").max(100,"Password shouldn't exceeds 100 character")
+});
+
