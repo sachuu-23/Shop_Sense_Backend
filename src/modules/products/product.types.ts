41f8533c5cde95c929d99313f0e1b38643042ad4
Sachin <sachinmizo1223@gmail.com>
2026-04-02 01:13:41 +0530

    Done some changes

--- src/modules/products/product.types.ts
+++ src/modules/products/product.types.ts
@@ -2,15 +2,24 @@
 //We started building our product feature from this file 
 
 export interface Product{
+    product_id : string,
     product_name : string,
     product_description : string | null,
     category : string,
     is_active : boolean, 
     created_at : Date,
     updated_at : Date
-}
+};
 
 export interface Product_Variant{
-    
+    product_variant_id : string,
+    product_id : string,
+    stock_keeping_unit :string,
+    product_size : string | null,
+    product_colour : string | null,
+    product_price : number,
+    is_active : boolean,
+    created_at : Date,
+    updated_at : Date,~;
 
-}
+};
