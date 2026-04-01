4be3652f654972ccfbf5b4c00e1ef15409be3834
Sachin <sachinmizo1223@gmail.com>
2026-04-01 21:30:10 +0530

    Some changes

--- src/config/schema.sql
+++ src/config/schema.sql
@@ -2,7 +2,7 @@
 
 --In this file we are going to write all our schema for this project , like users, prodcuts, refresh token, inventory etc , basically defiening the structure of how our data will be stored in our database and the relationship between those tables.
 
---USER TABLE 
+--USER TABLE ->
 
 CREATE TABLE IF NOT EXISTS users(
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),--uuid are strings
@@ -24,30 +24,26 @@ CREATE TABLE IF NOT EXISTS refresh_token(
    created_at TIMESTAMPTZ DEFAULT now()
 );
 
---PRODUCT TABLE 
-
+--PRODUCT TABLE ->
 CREATE TABLE IF NOT EXISTS product(
     product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    product_name VARCHAR(100) NOT NULL ,
-    category VARCHAR(100) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
     product_description TEXT ,
+    category VARCHAR(200) NOT NULL,
     is_active BOOLEAN DEFAULT true,
     created_at TIMESTAMPTZ DEFAULT now(),
-    updated_at TIMESTAMPTZ DEFAULT now()
+    updated_at TIMESTAMPTZ DEFAULT now(),
 
 );
-
 --PRODUCT VARIANT TABLE 
-
-CREATE TABLE IF NOT EXISTS product_variant(
-    product_variant_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+CREATE TABLE IF EXISTS product_variant(
+    product_variant_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),--PRIMARY KEY FOR VARIANT TABLE
     product_id UUID REFERENCES product(product_id) ON DELETE CASCADE,
-    stock_keeping_unit VARCHAR(100) UNIQUE NOT NULL ,
-    product_size VARCHAR(50),--this means it can be null or not 
+    stock_keeping_unit VARCHAR(255) UNIQUE NOT NULL,
+    product_size VARCHAR(50),
     product_colour VARCHAR(50),
-    product_price Numeric(10,2) NOT NULL,--here numeric(10,2 means that point left 10 digit is allowed and decimal right just 2 digit extra)
+    product_price Numeric(10,2) NOT NULL, --HERE 2 MEANS IN PRICE AFTER . ADD JUST 2 EXTRA NUMBER eg 45,000.00
     is_active BOOLEAN DEFAULT true,
-    created_at TIMESTAMPTZ DEFAULT now()
+    created_at TIMESTAMPTZ DEFUALT now(),
+    updated_at TIMESTAMPTZ DEFAULT now()
 );
---ON DELETE CASCADE MEANS IF THE SPECIFIC ID IN PARENT IS DELETED THEN DELETE HERE ALSO 
---product specifications is unique and null because for eg a product oppo f11 in its specifictaiton it will be like oppo f11, 8 gb ram , 256 gm rom and this so yeah this des should not be same with other property in the same column.
